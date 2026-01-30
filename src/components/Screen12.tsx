f4f3f774e2a3337d8d61a2b7598e0fb0d7d6ca7d
annaintend <anna@intend.com>
2026-01-30 14:52:46 +0200

    Update Screen12.tsx

--- src/components/Screen12.tsx
+++ src/components/Screen12.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import imgSugarnologooo1 from "figma:asset/90d0b2808b9f2d4ad23a49432895256cef99dbdf.png";
+import imgSugarnologooo1 from "@/assets/sugarno-black-logo.png";
 import img51 from "@/assets/Eating-outside.png";
 
 interface Screen12Props {
